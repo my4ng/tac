e9ddbf869e70dae5e0be03766a450989271febc4
Michael Yang <admin@my4ng.dev>
2024-05-28 02:30:40 +1000

    feat: use `clap` for command parsing
    Reduce increased binary size with release profile changes.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,7 @@
+use clap::{command, crate_authors, crate_description, crate_version, Arg, ArgAction};
 use tac_k::reverse_file;
 
 use std::io::{BufWriter, IsTerminal, StdoutLock, Write};
-
-fn version() {
-    println!("Tack {}", env!("CARGO_PKG_VERSION"));
-    println!("Copyright (c) 2024 Michael Yang <admin@my4ng.dev>");
-    println!("Copyright (c) 2017 NeoSmart Technologies <https://neosmart.net/>");
-    println!("Report bugs at <https://github.com/my4ng/tack>");
-}
-
-fn help() {
-    version();
-    println!();
-    println!("Usage: tac [OPTIONS] [FILE1..]");
-    println!("Write each FILE to standard output, last line first.");
-    println!("Reads from stdin if FILE is - or not specified.");
-    println!();
-    println!("Options:");
-    println!("  -h --help        Print this help text and exit");
-    println!("  -v --version     Print version and exit.");
-    println!("  --line-buffered  Always flush output after each line.");
-}
-
 enum Writer {
     StdOut(StdoutLock<'static>),
     Buffered(BufWriter<StdoutLock<'static>>),
@@ -43,47 +23,38 @@ impl Write for Writer {
     }
 }
 
-fn main() {
-    let args = std::env::args();
-    // This is intentionally one more than what we might need, in case no arguments were provided
-    // and we want to stub a "-" argument in there.
-    let mut files = Vec::with_capacity(args.len());
-    let mut force_flush = false;
-    let mut skip_switches = false;
-    for arg in args.skip(1) {
-        if !skip_switches && arg.starts_with('-') && arg != "-" {
-            match arg.as_str() {
-                "-h" | "--help" => {
-                    help();
-                    std::process::exit(0);
-                }
-                "-v" | "--version" => {
-                    version();
-                    std::process::exit(0);
-                }
-                "--line-buffered" => {
-                    force_flush = true;
-                }
-                "--" => {
-                    skip_switches = true;
-                    continue;
-                }
-                _ => {
-                    eprintln!("{}: Invalid option!", arg);
-                    eprintln!("Try 'tac --help' for more information");
-                    std::process::exit(-1);
-                }
-            }
-        } else {
-            let file = arg;
-            files.push(file)
-        }
-    }
+const HELP_TEMPLATE: &str = "\
+{name} ({version}) {author-with-newline}{about-with-newline}
+{usage-heading} {usage}
 
-    // Read from stdin by default
-    if files.is_empty() {
-        files.push("-".into());
-    }
+{all-args}";
+
+fn main() {
+    #[allow(non_upper_case_globals)]
+    let matches = command!()
+        .name("tac")
+        .about(crate_description!())
+        .author(crate_authors!("\n"))
+        .version(crate_version!())
+        .help_template(HELP_TEMPLATE)
+        .arg(
+            Arg::new("force_flush")
+                .long("line-buffered")
+                .action(ArgAction::SetTrue)
+                .help("Always flush output after each line"),
+        )
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .num_args(..)
+                .help("Files to be reversed.\nRead from stdin if it is `-` or not specified."),
+        )
+        .get_matches();
+
+    let force_flush = matches.get_flag("force_flush");
+    let files = matches
+        .get_many::<String>("files")
+        .map_or_else(|| vec!["-"], |iter| iter.map(AsRef::as_ref).collect());
 
     let stdout = std::io::stdout().lock();
     let mut writer = if force_flush || stdout.is_terminal() {
@@ -92,7 +63,7 @@ fn main() {
         Writer::Buffered(BufWriter::new(stdout))
     };
 
-    for file in &files {
+    for file in files {
         if let Err(e) = reverse_file(&mut writer, file) {
             if e.kind() != std::io::ErrorKind::BrokenPipe {
                 eprintln!("{}: {:?}", file, e);
